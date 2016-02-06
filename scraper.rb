91eb33968a4157a7f3b7f7bce11f55130d9d2cf3
Tony Bowden <tony@tmtm.com>
2016-02-06 15:34:57 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,13 @@
+require 'wikidata/fetcher'
+require 'pry'
+
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/House_of_Representatives_of_Fiji',
+  after: '//h2/span[@id="Open_Electorates"]',
+  before: '//h2/span[@id="See_also"]',
+  xpath: '//table//td[2]//a[not(@class="new")]/@title',
+  debug: true,
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
